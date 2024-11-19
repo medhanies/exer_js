7d464f011b4356d5b2154281eb7f1d290bf3e618
medhanies <medhaniesolomon@yahoo.com>
2024-11-19 14:40:49 -0600

    define constructor that defines position of window and a method that moves it across x and y plane

--- windowing-system/windowing-system.js
+++ windowing-system/windowing-system.js
@@ -15,3 +15,13 @@ Size.prototype.resize = function (newWidth, newHeight) {
   this.width = newWidth;
   this.height = newHeight;
 };
+
+export function Position(x = 0, y = 0) {
+  this.x = x;
+  this.y = y;
+}
+
+Position.prototype.move = function (newX, newY) {
+  this.x = newX;
+  this.y = newY;
+};
